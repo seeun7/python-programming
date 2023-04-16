7275965c98f982bc12e72ccb137ffe5baa2e44ba
seeun7 <126846674+seeun7@users.noreply.github.com>
2023-04-17 02:03:48 +0900

    ADD FILE

---
+++ hw6.py
@@ -0,0 +1,9 @@
+for i in range(1,10):
+  for j in range(2,6):
+    print(f'{j}x{i}={j*i:2d}\t',end='')
+  print()
+print()
+for i in range(1,10):
+  for j in range(6,10):
+    print(f'{j}x{i}={j*i:2d}\t',end='')
+  print()
